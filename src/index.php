25cbade4fbc8abe4c59fdba901a827042eaf339f
KEINOS <github@keinos.com>
2018-01-17 11:47:15 +0900

    CLI からの実行で引数を Qithub API に渡せるようにした
    CLI から `$ php qithub.phar help —version` の実行で `＠qithub:help —version`
    のトゥートと同等の処理になるようにした。
    `roll-dice` などは、まだ `args=` クエリの処理に対応していないため、動作しない。

--- src/index.php
+++ src/index.php
@@ -8,4 +8,49 @@
  * @author KEINOS( https://github.com/KEINOS )
  */
 
-echo "Hello world.";
+require_once('./constants.php.inc');
+require_once('./functions.php.inc');
+
+if (! is_mode_cli()) {
+    set_env_utf8_ja();
+    header('Content-Type: text/plain; charset=utf-8', true, 500);
+    echo 'Please execute it via php-cli.' . PHP_EOL;
+    die;
+}
+
+// Get args from CLI
+if (isset($argv[1])&& ! empty($argv[1])) {
+    $argv_tmp    = $argv;
+    $plugin_name = trim($argv_tmp[1]);
+    unset($argv_tmp[0]); // unset script name
+    unset($argv_tmp[1]); // unset 1st argument
+    $plugin_args = implode(' ', $argv_tmp); // Glue the rest args.
+} else {
+    $plugin_name = 'help';
+    $plugin_args = '';
+}
+
+// Qithub API Endpoint
+$url_endpoint = 'https://blog.keinos.com/qithub_dev/';
+
+// REST Request URL
+$plugin_name = urlencode($plugin_name);
+$plugin_args = urlencode($plugin_args);
+$url_request = "${url_endpoint}?plugin_name=${plugin_name}&args=${plugin_args}";
+
+// Get Qithub encoded data from Qithub API
+$response_raw = file_get_contents($url_request);
+
+// Decode Qithub encoded data to PHP array
+$response_json  = urldecode($response_raw);
+$response_array = json_decode($response_json, JSON_OBJECT_AS_ARRAY);
+
+// Show result(response)
+if ('OK' === $response_array['result']) {
+    echo $response_array['value'] . PHP_EOL;
+} else {
+    $value  = '';
+    $value .= print_pretty($url_request, 'Request query:', true);
+    $value .= print_pretty($response_array['value'], 'Value:', true);
+    print_pretty($value, 'Bad Request');
+}
